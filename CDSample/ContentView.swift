353801813a2a99aebe01cc689ca6c7502f47c4fa
roshan-tlc <roshan@thelaunchclub.in>
2025-06-12 11:00:48 +0530

    second change made

--- CDSample/ContentView.swift
+++ CDSample/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<3) { _ in
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
         }
         .padding()
     }

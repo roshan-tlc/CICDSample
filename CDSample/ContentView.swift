a6c0a646068d23c43302624ed315ee01862a33e2
roshan-tlc <roshan@thelaunchclub.in>
2025-06-17 15:00:23 +0530

    added lint warning

--- CDSample/ContentView.swift
+++ CDSample/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
                     .imageScale(.large)
                     .foregroundStyle(.tint)
                 Text("Hello, world!")
+                
+                Text("Welcome To Code")
             }
         }
         .padding()
